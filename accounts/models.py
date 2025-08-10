e0e8a050c0ed94825c57c52864ae4d7386b8ade4
sufiyan9828 <mohdsufiyan9828@gmail.com>
2025-08-10 18:39:47 +0530

    fifth commit

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,19 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
     dob = models.DateField(null=True, blank=True)  # Your custom field
 
     def __str__(self):
         return self.username
+
+class Address(models.Model):
+    title = models.CharField(max_length=40)
+    address_line_one = models.CharField(max_length=50)
+    address_line_two = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    pincode = models.IntegerField()
+    user = models.ForeignKey(User,on_delete=models.DO_NOTHING)
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
+from .models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
