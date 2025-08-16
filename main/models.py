7ebf3415dc0c0ef1db46e22e77ec59b6242b0d60
sufiyan9828 <mohdsufiyan9828@gmail.com>
2025-08-16 12:12:04 +0530

    tenth commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    job_title = models.CharField(max_length=25)
+    company_name = models.CharField(max_length=30)
+
+    job_type = models.CharField(max_length=1, choices=(
+        ('F','Full Time'),
+        ('P','Part Time'),
+        ('O','Other'),
+    ))
+
+    job_pay = models.CharField(max_length=1, choices=(
+        ('S','Salary'),
+        ('H','Hourly'),
+        ('O','Other'),
+    ))
+
+    company_logo = models.ImageField(null = True,blank = True)
+
+    location = models.CharField(max_length=50)
+
+    email_to = models.EmailField()
+
+    url_to = models.URLField()
